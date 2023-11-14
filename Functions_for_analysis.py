2f20d9b4fbabc0498b546342acb7a31653cea4a3
RosSlek <slekys.rosvaldas@gmail.com>
2023-11-14 20:23:19 +0200

    nauja versija

--- Functions_for_analysis.py
+++ Functions_for_analysis.py
@@ -17,7 +17,7 @@ def scrape_road_info_real_time():
     url = "https://eismoinfo.lt/eismoinfo-backend/osi-info-table"
 
     page = requests.get(url, headers=headers)
-
+    print(page.status_code)
     data = page.json()
 
     ############### DataFrame Clearance ###############
@@ -195,3 +195,67 @@ def avg_on_the_road_from_collected_data_by_day():
             print("Wrong input! I`ll take it as a No.")
 
 # avg_on_the_road_from_collected_data_by_day()
+
+############### Function To Scrape Road Traffic Intensity Real Time ###############
+def scrape_road_traffic_intensity_real_time():
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36",
+        'value': 'application/json, text/plain, */*',
+        'accept': 'application/json, text/plain, */*',
+        'cookie': '_ga=GA1.2.1272715160.1699462284; _gid=GA1.2.1647336826.1699462284; _ga_E8WJ6X0RYN=GS1.2.1699471615.3.0.1699471615.60.0.0; _ga_162SQ74LLR=GS1.2.1699471697.2.1.1699471698.0.0.0'
+    }
+
+    # https://eismoinfo.lt/#!/ information from page
+    url = "https://eismoinfo.lt/traffic-intensity-service"
+
+    page = requests.get(url, headers = headers)
+    print(page.status_code)
+    data = page.json()
+
+    ############### Creating DataFrames ###############
+
+    direction_positive = []
+    direction_negative = []
+
+    for i in range(len(data)):
+        try:
+            data2 = page.json()[i]['roadSegments'][0]
+            data3 = page.json()[i]['roadSegments'][1]
+            direction_positive.append(data2)
+            direction_negative.append(data3)
+        except IndexError:
+            direction_positive.append({'direction': None, 'startX': None, 'startY': None, 'endX': None, 'endY': None, 'winterSpeed': 0, 'summerSpeed': 0, 'numberOfVehicles': 0, 'averageSpeed': 0, 'trafficType': 0})
+            direction_negative.append({'direction': None, 'startX': None, 'startY': None, 'endX': None, 'endY': None, 'winterSpeed': 0, 'summerSpeed': 0, 'numberOfVehicles': 0, 'averageSpeed': 0, 'trafficType': 0})
+            continue
+
+    df_info = pd.DataFrame(data)
+    df_positive = pd.DataFrame(direction_positive)
+    df_negative = pd.DataFrame(direction_negative)
+
+    ############### DataFrame Clearance ###############
+
+    df_info['Date'] = df_info['date'].str.split(pat='.', n=0, expand=True)[0]
+    df_info['Date'] = df_info['Date'].str.replace('T', ' ')
+
+    df_info['Date'] = pd.to_datetime(df_info['Date'])
+    df_info['Date'] = df_info['Date'] + pd.Timedelta(hours=2)
+
+    df_info['Time'] = pd.to_datetime(df_info['Date']).dt.time
+    df_info['Date'] = pd.to_datetime(df_info['Date']).dt.date
+
+    df_info.drop(columns=['id', 'km', 'x', 'y', 'roadSegments', 'date'], inplace=True)
+    df_info.rename(columns={'name': 'Location', 'roadNr': 'Road', 'timeInterval': 'Time_Interval', 'roadName': 'Road_Name'}, inplace=True)
+    df_info['Road_Name'] = df_info['Road_Name'].str.replace('*', '')
+
+    df_positive.drop(columns=['startX', 'startY', 'endX', 'endY', 'winterSpeed', 'summerSpeed'], inplace=True)
+    df_positive.rename(columns={'direction': 'Direction_Positive', 'numberOfVehicles': 'Number_of_Vehicles', 'averageSpeed': 'Average_Speed', 'trafficType':'Traffic_Type'}, inplace=True)
+
+    df_negative.drop(columns=['startX', 'startY', 'endX', 'endY', 'winterSpeed', 'summerSpeed'], inplace=True)
+    df_negative.rename(columns={'direction': 'Direction_Negative', 'numberOfVehicles': 'Number_of_Vehicles', 'averageSpeed': 'Average_Speed', 'trafficType':'Traffic_Type'}, inplace=True)
+
+    ############### Joining DataFrames ###############
+
+    general = pd.concat([df_info, df_negative, df_positive], axis=1)
+    general.to_csv("Road_traffic_intensity_real_time.csv", index=False)
+
+scrape_road_traffic_intensity_real_time()
